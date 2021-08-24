fc66f41569e05cb9c09c877e9ad7b346a3d6a4e1
Alex Riley <alex.riley.ar12@gmail.com>
2021-08-24 11:13:09 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
-// helloWorld function 
+//helloWorld function
 function helloWorld() {
     return "Hello, World!";
 }
+//Refactor example
+// const helloWorld = function(){
+//     return "Hello, World!";
+// }
